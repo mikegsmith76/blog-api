16fe7a5942c7dc35796ffa53dab4f87608ee6451
Mike Smith <mail@mikegsmith.co.uk>
2019-07-05 22:35:28 +0100

    Remove emoji rubbish

--- web/app/themes/mikegsmith.uk/functions.php
+++ web/app/themes/mikegsmith.uk/functions.php
@@ -4,3 +4,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles(){
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
