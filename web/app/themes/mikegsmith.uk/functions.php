afbe44e5be95012d74ed1848fe763cf72e7f6c67
Mike Smith <mail@mikegsmith.co.uk>
2019-07-28 14:36:39 +0100

    Remove all but queueing styles

--- web/app/themes/mikegsmith.uk/functions.php
+++ web/app/themes/mikegsmith.uk/functions.php
@@ -1,15 +1,15 @@
 <?php
-remove_action('wp_head', 'print_emoji_detection_script', 7);
+/*remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action('admin_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');*/
 
 function enqueue_child_theme_styles(){
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 
-function clean_script_tag($input) {
+/*function clean_script_tag($input) {
     $input = str_replace("type='text/javascript' ", '', $input);
     $input = str_replace("type=\"text/javascript\" ", '', $input);
     return str_replace("'", '"', $input);
@@ -20,4 +20,4 @@ function clean_style_tag($input) {
     $input = str_replace('type="text/css" ', '', $input);
     return str_replace("'", '"', $input);
 }
-add_filter('style_loader_tag',  'clean_style_tag');
+add_filter('style_loader_tag',  'clean_style_tag');*/
